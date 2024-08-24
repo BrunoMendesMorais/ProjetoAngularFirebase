997b28df49783d4e0c977ac6262b9e89b4f5f4ff
Bruno mendes <162513613+BrunoMendesMorais@users.noreply.github.com>
2024-08-24 01:47:36 +0000

    badeiras

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -3,6 +3,7 @@ import { AuthenticateService } from '../services/auth.service';
 import { CrudService } from '../services/crud.service';
 import { Storage, getDownloadURL, ref, uploadBytesResumable } from '@angular/fire/storage';
 import { MessageService } from '../services/message.service';
+import { triggerAsyncId } from 'async_hooks';
 
 @Component({
   selector: 'app-home',
@@ -11,5 +12,23 @@ import { MessageService } from '../services/message.service';
 })
 export class HomePage {
   
+  paises: any;
+  isLoading: boolean = false;
+
+  constructor(){
+    this.getPaisCode();
+  }
+  getPaisCode(){
+    this.isLoading = true;
+    fetch('https://restcountries.com/v3.1/all?fields=name,ccn3,flags')
+    .then(dados => dados.json())
+    .then(dados => 
+      {
+      console.log(dados);
+      this.paises = dados;
+    })
+    .catch( erro => {console.log(erro)})
+    .finally( () => this.isLoading = false )
+  }
 
 }
